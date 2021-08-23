cddbf1ca18aa5ab3c65d37a6a85b531c0df1cb68
DerHamm <42356960+DerHamm@users.noreply.github.com>
2021-08-23 16:48:51 +0200

    Now saves the price history in a csv
    Maybe not the best format, because the file will sooner or later become too big in size.
    But we can always upgrade to a database or another solution when this happens.

--- live.py
+++ live.py
@@ -13,7 +13,19 @@ from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 
 COINS = 'bitcoin'
-REFRESH_INTERVAL = 60
+# in seconds
+REFRESH_INTERVAL = 62
+
+
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+#fig = plotly.graph_objects.Figure(data=[plotly.graph_objects.Candlestick(x=df['Date'],
+#                open=df['AAPL.Open'],
+#                high=df['AAPL.High'],
+#                low=df['AAPL.Low'],
+#                close=df['AAPL.Close'])])
+
+
 
 #chart = cg.get_coin_market_chart_by_id(id=COINS, vs_currency='usd', days=14)
 #good_chart = []
@@ -22,7 +34,6 @@ REFRESH_INTERVAL = 60
 #df = pd.DataFrame(good_chart)
 #df.rename(columns={0: 'timestamp', 1: 'bitcoin'}, inplace=True)
 #INITIAL_FRAME = df
-
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -53,9 +64,16 @@ app.layout = html.Div(
 #        html.Span('Altitude: {0:0.2f}'.format(alt), style=style)
 #    ]
 
-prices = cg.get_price(ids=COINS, vs_currencies='usd')
-prices['timestamp'] = datetime.datetime.now()
-data = pd.DataFrame(prices)
+
+from pathlib import Path
+
+csv = Path('data.csv')
+if csv.is_file():
+    data = pd.read_csv(csv)
+else:
+    prices = cg.get_price(ids=COINS, vs_currencies='usd')
+    prices['timestamp'] = datetime.datetime.now()
+    data = pd.DataFrame(prices)
 
 # Multiple components can update everytime interval gets fired.
 @app.callback(Output('live-update-graph', 'figure'),
@@ -67,9 +85,15 @@ def update_graph_live(n):
     df = pd.DataFrame(prices)
 
 
+    record_count = len(data.index)
     data = data.append(df)
-    fig = px.line(data, x='timestamp', y='bitcoin')
+    if len(data.index) > record_count:
+        data.to_csv('data.csv', mode="w")
+    else:
+        print('data was lost, trying to read from csv')
+        data = pd.read_csv(csv)
 
+    fig = px.line(data, x='timestamp', y='bitcoin')
     # keep zoom
     fig['layout']['uirevision'] = True
 
